c9f2718a1506987226211e0d60981d5f9c36b6d9
costafelipe452 <felipe.452@hotmail.com>
2023-03-16 00:56:15 -0300

    final curso 1

---
+++ galeria/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def index(requests):
+    return render(requests,'galeria/index.html')
+
+def imagem(requests):
+    return render(requests, 'galeria/imagem.html')
+
